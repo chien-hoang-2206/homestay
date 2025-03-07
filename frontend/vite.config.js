09456c2648e82e3b56bdf7b1a7087ed49e779bfc
chienhv <hoangvanchien2206@gmail.com>
2025-03-08 00:52:58 +0700

    fix: config server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,10 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
+import dotenv from 'dotenv';
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
 export default defineConfig({
     plugins: [react()],
     resolve: {
